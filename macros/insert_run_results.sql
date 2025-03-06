0378045ef7271185550f2b5dce47fde1b4a2ccf4
sumansatpathy1 <110736446+sumansatpathy1@users.noreply.github.com>
2025-03-06 23:12:39 +0530

    Update insert_run_results.sql

--- macros/insert_run_results.sql
+++ macros/insert_run_results.sql
@@ -1,20 +1,24 @@
 {% macro insert_run_results() %}
-{% set run_results = load_result('target/run_results.json').results %}
-{% for result in run_results %}
-INSERT INTO dbtdemo.dbt_run_results (
-    node_id,
-    node_name,
-    node_type,
-    status,
-    execution_time,
-    invocation_id
-) VALUES (
-    '{{ result.node.unique_id }}',
-    '{{ result.node.name }}',
-    '{{ result.node.resource_type }}',
-    '{{ result.status }}',
-    '{{ result.execution_time }}',
-    '{{ invocation_id }}'
-);
-{% endfor %}
+  {% set run_results = load_result('target/run_results.json').results %}
+  {% if run_results %}
+    {% for result in run_results %}
+      INSERT INTO dbtdemo.dbt_run_results (
+          node_id,
+          node_name,
+          node_type,
+          status,
+          execution_time,
+          invocation_id
+      ) VALUES (
+          '{{ result.node.unique_id }}',
+          '{{ result.node.name }}',
+          '{{ result.node.resource_type }}',
+          '{{ result.status }}',
+          '{{ result.execution_time }}',
+          '{{ invocation_id }}'
+      );
+    {% endfor %}
+  {% else %}
+    {{ log("Warning: run_results.json not found or empty.", info=True) }}
+  {% endif %}
 {% endmacro %}
