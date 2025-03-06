6a124161683772c9d869c25db4404d864ed8cc99
sumansatpathy1 <110736446+sumansatpathy1@users.noreply.github.com>
2025-03-06 23:10:18 +0530

    Update insert_run_results.sql

--- macros/insert_run_results.sql
+++ macros/insert_run_results.sql
@@ -1,7 +1,7 @@
 {% macro insert_run_results() %}
 {% set run_results = load_result('target/run_results.json').results %}
 {% for result in run_results %}
-INSERT INTO dbt-demo.dbt_run_results (
+INSERT INTO dbtdemo.dbt_run_results (
     node_id,
     node_name,
     node_type,
